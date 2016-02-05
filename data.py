481665f38f9d6c20cba6e00dd5221245708fbadc
s.zharov <s.zharov_1990@mail.ru>
2016-02-05 18:10:11 +1000

    -- initial commit

---
+++ data.py
@@ -0,0 +1,16 @@
+import sys
+import os.path
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    if len(sys.argv[1]) > 0:
+        if os.path.isfile(sys.argv[0]):
+            file = open(sys.argv[1], '+r', encoding='utf-8')
+            for row in file:
+                file.writelines(row.replace(" ", ", ", 1))
+                print(row)
+                # something new
+                # test
+            file.close();
+    else:
+        print("Enter filename")
